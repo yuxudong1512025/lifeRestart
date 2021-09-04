9559be24c317c30f689fabf1019cb45ac6d3f6ac
yuxudong <yuxd2@spdb.com.cn>
2021-09-04 21:06:26 +0800

    add dev server run config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const path = require('path');
 
+const webpack = require('webpack');
+
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: './src/index.js',
+  entry: path.join(__dirname, './src/index.js'),
   mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, './public'),
     filename: 'bundle.js',
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname),
+    },
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new htmlWebpackPlugin({
+      template: path.join(__dirname, './view/index.html'),
+      filename: 'index.html'
+    })
+  ],
   // resolve: {
   //   extensions: [".js"],
   // },
